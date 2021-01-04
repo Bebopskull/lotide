9ba4f49153124378397fad094d8612f1165fbd56
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-04 18:45:29 -0500

    adding head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('🤦🤦🤦 Assertion Failed: ' + actual + ' !== ' + expected)
+  } else {
+    console.log('😎😎😎 Assertion Passed: ' + actual + ' === ' + expected)
+  }
+}
+
+
+
+let head = function(array){
+  return array[0];
+
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
