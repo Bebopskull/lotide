b85c35db191786c7fa56341a8f1b03985f1fd916
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-04 14:31:39 -0500

    update 2

--- assertEqual.js
+++ assertEqual.js
@@ -1,26 +1,22 @@
-const assertEqual = function(actual, expected) {
-
-  let assert=null;
-
-  if(actual !== expected){
-    console.log('🤦🤦🤦 Assertion Failed: '+actual+' !== '+ expected)
-  }else{
-    console.log('😎😎😎 Assertion Passed: '+actual+' === ' +expected)
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('🤦🤦🤦 Assertion Failed: ' + actual + ' !== ' + expected)
+  } else {
+    console.log('😎😎😎 Assertion Passed: ' + actual + ' === ' + expected)
   }
-
-};
+}
 /*
 🛑🛑🛑 Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
 ✅✅✅ Assertion Passed: 1 === 1
 */
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual('Lighthouse Labs', 'Bootcamp')
+assertEqual(1, 1)
 
-assertEqual("Hello","Hello");
+assertEqual('Hello', 'Hello')
 
-assertEqual('Hello',"hello");
+assertEqual('Hello', 'hello')
 
-assertEqual(3,3);
+assertEqual(3, 3)
 
-assertEqual(3,30);
+assertEqual(3, 30)
