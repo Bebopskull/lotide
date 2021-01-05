c49cabc98b7a1b3891800817278fe39ccfdaff57
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-04 19:03:02 -0500

    adding tail.js

---
+++ tail.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('🤦🤦🤦 Assertion Failed: ' + actual + ' !== ' + expected)
+  } else {
+    console.log('😎😎😎 Assertion Passed: ' + actual + ' === ' + expected)
+  }
+}
+
+let tail = function(array){
+
+  let decapitated = array.slice(1)
+
+  return decapitated
+
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
+/*
+
+ So, In order to make it work
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: ...
+// ...
+
+*/
