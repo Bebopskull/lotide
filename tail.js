47f4c43bd47a1ed5afeb0728a250d299438e5cf2
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-11 13:48:10 -0500

    refactor tail.js and added tailTest.js

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log('🤦🤦🤦 Assertion Failed: ' + actual + ' !== ' + expected)
-  } else {
-    console.log('😎😎😎 Assertion Passed: ' + actual + ' === ' + expected)
-  }
-}
+const assertEqual = require('./assertEqual')
 
 let tail = function(array){
 
@@ -14,22 +8,11 @@ let tail = function(array){
 
 }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-/*
 
- So, In order to make it work
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+//  So, In order to make it work
 
 // Test Case 2: ...
 // ...
 
-*/
---
+++ test/tailTest.js
@@ -0,0 +1,20 @@
+///imports
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+/////tests
+
+
+let result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
+
+// Test Case 1: Check the returned array elements
+result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
